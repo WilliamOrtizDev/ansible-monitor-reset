5270bdf50514b9143bd6ef1f0ad6bcc607b9d43d
William Ortiz <willyortiz93@gmail.com>
2023-07-13 18:11:30 -0400

    Update monitor_check.sh
    Checks for all output types

--- roles/monitor_reset/files/monitor_check.sh
+++ roles/monitor_reset/files/monitor_check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DISPLAY_PORTS=$(find /sys/class/drm | grep "DP-." | cut -d '-' -f 2,3)
+DISPLAY_PORTS=$(find /sys/class/drm | grep ".-." | cut -d '-' -f 2,3)
 
 for i in $DISPLAY_PORTS; do
     DISPLAY=$i;
